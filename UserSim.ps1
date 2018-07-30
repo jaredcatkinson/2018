f6a8c3f0cedb3dd902ca54b1f9edeabffe54025e
Jared Atkinson <jaredcatkinson@users.noreply.github.com>
2018-07-30 09:26:15 -0700

    Update UserSim.ps1

--- UserSim.ps1
+++ UserSim.ps1
@@ -16,9 +16,9 @@ schtasks /create /tn Start_User_Sim /sc ONLOGON /tr "schtasks /create /ru specte
 
 $runonce = 'HKLM:\\\\SOFTWARE\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run'
 Set-ItemProperty $runonce Test "echo text > C:\Users\Public\usersim.txt"
-Set-ItemProperty $runonce Start_User_Sim_Browsing "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Browsing /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserDocument.exe /f"
-Set-ItemProperty $runonce Start_User_Sim_Document "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Document /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserDocument.exe /f"
-Set-ItemProperty $runonce Start_User_Sim_PowerShell "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserPowerShell.exe /f"
+Set-ItemProperty $runonce Start_User_Sim_Browsing "schtasks /create /u testadmin /p P@ssw0rd! /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Browsing /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserDocument.exe /f"
+Set-ItemProperty $runonce Start_User_Sim_Document "schtasks /create /u specterpops\corpadmin /p corpP@ssw0rd! /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Document /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserDocument.exe /f"
+Set-ItemProperty $runonce Start_User_Sim_PowerShell "schtasks /create /u specterpops\$($username) /p $($password) /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserPowerShell.exe /f"
 Set-ItemProperty $runonce Start_User_Sim_WebMail "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Mail /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserWebMail.exe /f" 
 
 
