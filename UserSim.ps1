ba85af6d00cccf93887b3b19e0c5e9c81ce8bb5f
Jared Atkinson <jaredcatkinson@users.noreply.github.com>
2018-07-31 21:08:59 -0700

    Update UserSim.ps1

--- UserSim.ps1
+++ UserSim.ps1
@@ -4,31 +4,22 @@ param
     $password
 )
 
-"Adding AutoIT_Browsing Task" | Out-File -FilePath C:\Users\Public\usersim.txt -Append
-schtasks /create /tn Start_User_Sim /sc ONLOGON /tr "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Browsing /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserBrowsing.exe /f" /f
-"Adding AutoIT_Document Task" | Out-File -FilePath C:\Users\Public\usersim.txt -Append
-schtasks /create /tn Start_User_Sim /sc ONLOGON /tr "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Document /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserDocument.exe /f" /f
-"Adding AutoIT_PowerShell Task" | Out-File -FilePath C:\Users\Public\usersim.txt -Append
-schtasks /create /tn Start_User_Sim /sc ONLOGON /tr "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserPowerShell.exe /f" /f
-"Adding AutoIT_Mail Task" | Out-File -FilePath C:\Users\Public\usersim.txt -Append
-schtasks /create /tn Start_User_Sim /sc ONLOGON /tr "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Mail /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserWebMail.exe /f" /f
-
 $run = 'HKLM:\\\\SOFTWARE\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run'
 $admins = @("marilyn.adams", "aubree.jenkins", "daisy.allen", "zoey.shaw", "luis.sanders", "beverly.vargas", "sarah.wells", "melanie.gutierrez", "rosa.jordan", "eugene.sanders", "troy.williamson", "melissa.nichols", "ruben.roberts", "micheal.patterson", "kaylee.edwards", "raul.garcia", "edgar.stone", "joyce.reid", "dianne.brown", "owen.warren", "robin.armstrong", "sebastian.mitchell", "shane.patterson", "gregory.kelley", "nathan.lopez", "olivia.diaz", "adam.gonzales", "marsha.fisher", "luis.snyder", "eleanor.bates")
 if($admins -contains $username)
 { 
     Set-ItemProperty $run Start_Admin_Sim_Browsing "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Browsing /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminBrowsing.exe /f"
-    Set-ItemProperty $run Start_Admin_Sim_Commands "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Commands /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminCommands.exe /f"
-    Set-ItemProperty $run Start_Admin_Sim_PowerShell "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminPowerShell.exe /f"
-    Set-ItemProperty $run Start_Admin_Sim_ProcMon "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_ProcMon /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminProcMon.exe /f" 
-    Set-ItemProperty $run Start_Admin_Sim_RDP "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_RDP /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminRDP.exe /f" 
+    Set-ItemProperty $run Start_Admin_Sim_Commands "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Commands /sc MINUTE /mo 10 /tr C:\Users\Public\AdminActions\AdminCommands.exe /f"
+    #Set-ItemProperty $run Start_Admin_Sim_PowerShell "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminPowerShell.exe /f"
+    Set-ItemProperty $run Start_Admin_Sim_ProcMon "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_ProcMon /sc MINUTE /mo 42 /tr C:\Users\Public\AdminActions\AdminProcMon.exe /f" 
+    #Set-ItemProperty $run Start_Admin_Sim_RDP "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_RDP /sc MINUTE /mo 5 /tr C:\Users\Public\AdminActions\AdminRDP.exe /f" 
 }
 else
 {
-    Set-ItemProperty $run Start_User_Sim_Browsing "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Browsing /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserBrowsing.exe /f"
-    Set-ItemProperty $run Start_User_Sim_Document "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Document /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserDocument.exe /f"
-    Set-ItemProperty $run Start_User_Sim_PowerShell "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserPowerShell.exe /f"
-    Set-ItemProperty $run Start_User_Sim_WebMail "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Mail /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserWebMail.exe /f" 
+    Set-ItemProperty $run Start_User_Sim_Browsing "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Browsing /sc MINUTE /mo 7 /tr C:\Users\Public\UserActions\UserBrowsing.exe /f"
+    Set-ItemProperty $run Start_User_Sim_Document "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Document /sc MINUTE /mo 20 /tr C:\Users\Public\UserActions\UserDocument.exe /f"
+    #Set-ItemProperty $run Start_User_Sim_PowerShell "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_PowerShell /sc MINUTE /mo 5 /tr C:\Users\Public\UserActions\UserPowerShell.exe /f"
+    Set-ItemProperty $run Start_User_Sim_WebMail "schtasks /create /ru specterpops\$($username) /rp $($password) /it /tn AutoIT_Mail /sc MINUTE /mo 30 /tr C:\Users\Public\UserActions\UserWebMail.exe /f" 
 }
 
 "Setting Auto Logon User" | Out-File -FilePath C:\Users\Public\usersim.txt -Append
